9f5c2a0ffc56aedb004f887323c7d3ea0ceec2e2
alan <alan_yeh@mail.chungyo.net>
2022-03-16 15:11:05 +0800

    [MOD] config 寫法調整以利本地開發以及vendor引用

--- config/config.go
+++ config/config.go
@@ -24,11 +24,15 @@ type ProviderInfo struct {
 
 func GetProviderInfo(fileName string) *ProviderInfo {
 
-	currentPath, err := os.Getwd()
-	fmt.Println(currentPath)
+	currentPath, _ := os.Getwd()
 	// viper.AddConfigPath("gitlab.inlive7.com/crypto/ethereum-relay/config/")
 	// viper.AddConfigPath(currentPath)
-	viper.SetConfigFile(path.Join(currentPath, "config", fileName))
+	fullpath := path.Join(currentPath, "config", fileName)
+	_, err := os.Stat(fullpath)
+	if err != nil {
+		fullpath = path.Join(currentPath, "vendor/gitlab.inlive7.com/crypto/ethereum-relay/config/", fileName)
+	}
+	viper.SetConfigFile(fullpath)
 	viper.SetConfigType("yml")
 	// viper.SetConfigName(fileName)
 	// viper.SetConfigType("yml")
